98ba8b029c1d2e4203897a121e98f291ba59bbf4
Nirwash <nirwash@bk.ru>
2022-08-02 09:39:01 +0300

    заменил Timer на тестовую реализацию

--- app/src/test/java/com/nirwashh/android/viewmodeltest/viewmodel/ModelTest.kt
+++ app/src/test/java/com/nirwashh/android/viewmodeltest/viewmodel/ModelTest.kt
@@ -23,34 +23,66 @@ class ModelTest {
         override fun getInt(key: String) = int
     }
 
+    private class TestTimeTicker : TimeTicker  {
+        private var callback: TimeTicker.Callback? = null
+
+        var state = 0
+
+        override fun start(callback: TimeTicker.Callback, period: Long) {
+            this.callback = callback
+            state = 1
+        }
+
+        override fun stop() {
+            callback = null
+            state = -1
+        }
+
+        fun tick(times: Int) {
+            for (i in 0 until times) {
+                callback?.tick()
+            }
+        }
+    }
+
     @Test
     fun test_start_with_saved_value() {
         val testDataSource = TestDataSource()
-        val model = Model(testDataSource)
+        val timeTicker = TestTimeTicker()
+        val model = Model(testDataSource, timeTicker)
         val callback = TestCallback()
         testDataSource.saveInt("", 5)
         model.start(callback)
-        Thread.sleep(5)
+        timeTicker.tick(1)
         val actual = callback.text
-        val expected = "5"
+        val expected = "6"
         assertEquals(expected, actual)
     }
 
     @Test
     fun test_stop_after_2_seconds() {
         val testDataSource = TestDataSource()
-        val model = Model(testDataSource)
+        val timeTicker = TestTimeTicker()
+        val model = Model(testDataSource, timeTicker)
         val callback = TestCallback()
         testDataSource.saveInt("", 0)
         model.start(callback)
-        Thread.sleep(2010)
+        timeTicker.tick(2)
         val actual = callback.text
         val expected = "2"
         assertEquals(expected, actual)
+
+
         model.stop()
         val savedCountActual = testDataSource.getInt("")
         val savedCountExpected = 2
         assertEquals(savedCountExpected,savedCountActual)
+
+        model.start(callback)
+        timeTicker.tick(3)
+        val actualText = callback.text
+        val expectedText = "5"
+        assertEquals(expectedText, actualText)
     }
 }
 
